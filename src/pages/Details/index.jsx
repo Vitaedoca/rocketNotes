fd22762d96b91b26bb529f9c8e539bc43090cae3
Vinicius Gabriel <viniciusgabrielpe@gmail.com>
2023-08-14 12:26:24 -0300

    feat(Primeiro commit)

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from './styles';
+
+export function Details() {
+
+  return(
+      <Container>
+        <h1>Hello Word!</h1>
+        <h2>Olá mundo</h2>
+      </Container>
+
+  )
+
+}
